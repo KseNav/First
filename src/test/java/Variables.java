d449e65b34fd20277610d39bc4232cf0ef930bc8
KseNav <ksuk4you.ru@gmail.com>
2021-09-08 20:07:38 +0300

    Variables

---
+++ src/test/java/Variables.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+public class Variables {
+    public int a = 2;
+    public int b = 3;
+
+    @Test
+    public void calculate() {
+        int c = a + b;
+        System.out.println("Результат сложения чисел = " + (a + b));
+    }
+}
